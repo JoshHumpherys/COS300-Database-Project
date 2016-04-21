1553be1bf453f353849671bd756b962d0d27f5bb
Johnathon Kelley <Johnathon Kelley>
2016-04-21 00:08:22 -0400

    Fixed Skeleton of Service Page

--- service.php
+++ service.php
@@ -3,25 +3,77 @@
 <?php require "header.php"; ?>
 <body>
 <div class="container">
+    <h1 align="center">Marcia's Dry Cleaning Services</h1>
+    <br>
     <form action="service.php" method="post">
-        <select name="action_type">
-            <option value="add">Add</option>
-            <option value="update">Update</option>
-            <option value="delete">Delete</option>
-        </select>
-        Cust Id: <input type="number" , name="CustId" , min="0" , max="999" , step="1">
-        First Name: <input type="text" , name="FirstName" size="35">
-        Last Name: <input type="text" , name="LastName" , size="35">
-        <br>
-        Address: <input type="text" , name="Address" , size="35">
-        City: <input type="text" , name="City" , size="15">
-        Zip: <input type="text" , name="Zip" , size="5">
-        Phone: <input type="text" , name="Phone" , size="10">
+        <ul style="text-align:left; list-style-type:none; display:block;font-size:20px;">
+            <li>Options: 
+                <select name="action_type">
+                    <option value="add">Add</option>
+                    <option value="update">Update</option>
+                    <option value="delete">Delete</option>
+                </select>
+            </li>
+            <li>Service ID: <input type="number" , name="serviceID" , min="0" , max="999" , step="1"></li>
+            <li>Description: <input type="text" , name="description" size="35"></li>
+            <li>Price: <input type="number" , name="price" , size="5"></li>
+            <li>Hours Required: <input type="number" , name="hours" , size="5"></li>
+        </ul>
+        
         <input type="submit" value="Send">
+        <input type="button" value="View All">
     </form>
     <?php
     // set server access variables
+        $host = "localhost";
+        $user = "Hunt";
+        $pass = "PW300";
+        $db = "marciadb";
+        $mysqli = new mysqli($host, $user, $pass, $db);
 
+        // check for connection errors
+        if (mysqli_connect_errno()) {
+            die("Unable to connect!");
+        }
+
+        // create query
+        $query = "SELECT * FROM service";
+
+        // execute query
+        if ($result = $mysqli->query($query)) {
+            // see if any rows were returned
+            if ($result->num_rows > 0) {
+                // yes
+                // print them one after another
+                echo "<table cellpadding=10 border=1>";
+                echo "<tr>";
+                echo "<th>ID</th>";
+                echo "<th>Country</th>";
+                echo "<th>Animal</th>";
+                echo "</tr>";
+                while($row = $result->fetch_array()) {
+                    echo "<tr>";
+                    echo "<td>".$row[0]."</td>";
+                    echo "<td>".$row[1]."</td>";
+                    echo "<td>".$row[2]."</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
+            else {
+                // no
+                // print status message
+                echo "No rows found!";
+            }
+
+            // free result set memory
+            $result->close();
+        }else {
+    // print error message
+    echo "Error in query: $query. ".$mysqli->error;
+}
+// close connection
+$mysqli->close();
     ?>
 </div>
 </body>
