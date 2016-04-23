7d6e455f5dce6b58c93eba00e195e2576e0ac302
Josh Humpherys <joshua.humpherys@gmail.com>
2016-04-23 14:00:22 -0400

    Create mysql script to insert fake data

---
+++ insert_test_data.sql
@@ -0,0 +1,95 @@
+INSERT INTO `service` VALUES (DEFAULT, 'Wash shirt (men)', 1.85, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash tuxedo shirt', 4.20, 5);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash shirt (women)', 5.00, 4);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash pants', 5.00, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash shorts', 4.50, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash golf shirt', 4.50, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash sweater', 5.00, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash lab coat', 7.00, 4);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash boxed shorts', 2.50, 2);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean 2 piece suit', 9.50, 15);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean tuxedo suit', 10.50, 16);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean 3 piece suit', 12.50, 18);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean blazer', 6.50, 8);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean dress', 9.50, 9);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean dress silk', 12.00, 11);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean 2 piece dress', 13.50, 12);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean vest', 4.50, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean sweater - regular', 5.00, 4);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean sweater - cashmere', 6.00, 5);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean sweater - thick', 6.25, 5);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean tank top', 4.35, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean robe', 12.00, 4);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean rain coat', 13.00, 6);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean rain coat with lining', 15.00, 6);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean 3/4 length coat', 8.00, 5);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean long coat', 15.00, 5);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean heavy jacket/ short coat', 8.00, 8);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean skirt', 5.00, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean skirt suit', 9.00, 8);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean evening dress', 6.50, 5);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean tie', 4.00, 2);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean scarf', 6.00, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean cap', 5.00, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean glove', 6.00, 2);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean blouse (regular)', 5.00, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean graduation gown/choir robe', 13.00, 8);
+INSERT INTO `service` VALUES (DEFAULT, 'Dry clean thick gown/clergy robe', 18.00, 9);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash comforter full', 20.00, 4);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash comforter queen/king', 25.00, 4);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash comforter (down)', 30.00, 4);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash blanket', 16.00, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash quilt', 20.00, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash pillow cover', 3.00, 2);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash sheet (each)', 13.00, 5);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash table cloth, skirt', 18.00, 4);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash napkin', 1.80, 2);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash apron', 6.00, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash curtain', 10.00, 4);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash curtain with lining', 15.00, 6);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash cushion', 12.00, 5);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash cushion cover', 6.00, 3);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash sofa cover', 8.00, 5);
+INSERT INTO `service` VALUES (DEFAULT, 'Wash sleeping bag', 20.00, 8);
+
+INSERT INTO `customer` VALUES (DEFAULT, 'Rene', 'Parker', 'reneparker22@gmail.com', '388 Warren Street
+Dekalb, IL 60115', DEFAULT);
+INSERT INTO `customer` VALUES (DEFAULT, 'Bobby', 'Weber', 'bobby.weber@gmail.com', '5175 Cambridge Court
+Greenville, NC 27834', DEFAULT);
+INSERT INTO `customer` VALUES (DEFAULT, 'Marcos', 'Green', 'marcos.green@yahoo.com', '5789 Franklin Court
+Stow, OH 44224', TRUE);
+INSERT INTO `customer` VALUES (DEFAULT, 'Clay', 'Cooper', 'claycooper@gmail.com', '131 Chapel Street
+Livingston, NJ 07039', DEFAULT);
+INSERT INTO `customer` VALUES (DEFAULT, 'Ian', 'Webb', 'ian.webb@gmail.com', '2139 Valley View Drive
+Vincentown, NJ 08088', DEFAULT);
+INSERT INTO `customer` VALUES (DEFAULT, 'Darryl', 'Sutton', 'darrylmsutton@gmail.com', '9044 Route 4
+Waterbury, CT 06705', TRUE);
+INSERT INTO `customer` VALUES (DEFAULT, 'Bridget', 'Miller', 'bmiller@outlook.com', '81 Court Street
+Huntsville, AL 35803', TRUE);
+INSERT INTO `customer` VALUES (DEFAULT, 'John', 'Norton', 'johnnorton1112@yahoo.com', '494 Smith Street
+Mountain View, CA 94043', TRUE);
+INSERT INTO `customer` VALUES (DEFAULT, 'Cody', 'Buchanan', 'cody.buchanan@gmail.com', '762 4th Street North
+Mount Juliet, TN 37122', DEFAULT);
+INSERT INTO `customer` VALUES (DEFAULT, 'Mitchell', 'Steele', 'mitchells@yahoo.com', '935 Cleveland Street
+Silver Spring, MD 20901', DEFAULT);
+
+INSERT INTO `phone` VALUES(1, '(815) 437-0507');
+INSERT INTO `phone` VALUES(1, '(815) 182-9858');
+INSERT INTO `phone` VALUES(2, '(252) 845-5811');
+INSERT INTO `phone` VALUES(3, '(234) 867-8349');
+INSERT INTO `phone` VALUES(4, '(862) 986-0316');
+INSERT INTO `phone` VALUES(5, '(609) 330-5510');
+INSERT INTO `phone` VALUES(5, '(609) 107-8259');
+INSERT INTO `phone` VALUES(5, '(609) 264-6042');
+INSERT INTO `phone` VALUES(6, '(203) 179-8157');
+INSERT INTO `phone` VALUES(7, '(256) 728-1951');
+INSERT INTO `phone` VALUES(8, '(650) 427-0643');
+INSERT INTO `phone` VALUES(8, '(650) 563-8312');
+INSERT INTO `phone` VALUES(9, '(615) 868-5165');
+INSERT INTO `phone` VALUES(10, '(240) 553-5636');
+
+INSERT INTO `order` VALUES(DEFAULT, '2016-04-19', NULL, NULL, NULL, 1);
+INSERT INTO `order_item` VALUES(DEFAULT, 3, NULL, NULL);
+INSERT INTO `order_item` VALUES(DEFAULT, 1, NULL, NULL);
+INSERT INTO `order_item_has_service` VALUES(1, 1);
+INSERT INTO `order_item_has_service` VALUES(1, 2);
