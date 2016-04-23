1b5c7ef2c7eb85c23df2b04fb29a4cfc5d189115
Josh Humpherys <joshua.humpherys@gmail.com>
2016-04-22 23:22:52 -0400

    Populate customer table with data from database

--- customer.php
+++ customer.php
@@ -6,7 +6,7 @@
     <h2>Customers</h2>
     <div class="table-responsive">
         <table class="table table-bordered table-striped">
-            <thead>
+            <tr>
                 <th>First Name</th>
                 <th>Last Name</th>
                 <th>Phone</th>
@@ -15,63 +15,64 @@
                 <th>Member</th>
                 <th>Edit</th>
                 <th>Delete</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Josh</td>
-                    <td>Humpherys</td>
-                    <td>800-555-1234</td>
-                    <td>joshua.humpherys@gmail.com</td>
-                    <td>256 Hello World St</td>
-                    <td>Yes</td>
-                    <td>
-                        <button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit">
-                            <span class="glyphicon glyphicon-pencil"></span>
-                        </button>
-                    </td>
-                    <td>
-                        <button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete">
-                            <span class="glyphicon glyphicon-trash"></span>
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>David</td>
-                    <td>Reed</td>
-                    <td>706-555-8882</td>
-                    <td>david.reed@foobarbaz.com</td>
-                    <td>128 Asdf Rd</td>
-                    <td>No</td>
-                    <td>
-                        <button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit">
-                            <span class="glyphicon glyphicon-pencil"></span>
-                        </button>
-                    </td>
-                    <td>
-                        <button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete">
-                            <span class="glyphicon glyphicon-trash"></span>
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Josh</td>
-                    <td>Humpherys</td>
-                    <td>800-555-1234</td>
-                    <td>joshua.humpherys@gmail.com</td>
-                    <td>1024 Binary Dr</td>
-                    <td>Yes</td>
-                    <td>
-                        <button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit">
-                            <span class="glyphicon glyphicon-pencil"></span>
-                        </button>
-                    </td>
-                    <td>
-                        <button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete">
-                            <span class="glyphicon glyphicon-trash"></span>
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
+            </tr>
+            <?php
+            $host = "localhost";
+            $user = "Hunt";
+            $pass = "PW300";
+            $db = "marciadb";
+            $mysqli = new mysqli($host, $user, $pass, $db);
+            if (mysqli_connect_errno()) {
+                die("Unable to connect!");
+            }
+
+            $columns = "customer.CustomerID, FirstName, LastName, Email, Address, HasMembership, Phone";
+            $query = "SELECT ".$columns." FROM customer LEFT JOIN phone ON customer.CustomerID = phone.CustomerID";
+            $CUSTOMER_ID_INDEX = 0;
+            $FIRST_NAME_INDEX = 1;
+            $LAST_NAME_INDEX = 2;
+            $EMAIL_INDEX = 3;
+            $ADDRESS_INDEX = 4;
+            $HAS_MEMBERSHIP_INDEX = 5;
+            $PHONE_INDEX = 6;
+
+            $lastID = -1;
+            $result = $mysqli->query($query);
+            while ($row = $result->fetch_array()) {
+                $id = $row[$CUSTOMER_ID_INDEX];
+                $firstName = $row[$FIRST_NAME_INDEX];
+                $lastName = $row[$LAST_NAME_INDEX];
+                $email = $row[$EMAIL_INDEX];
+                $address = $row[$ADDRESS_INDEX];
+                $member = $row[$HAS_MEMBERSHIP_INDEX];
+                $phone = $row[$PHONE_INDEX];
+                if($id == $lastID) {
+                    $firstName = $lastName = $email = $address = '';
+                    $member = -1;
+                }
+                $lastID = $id;
+                echo '
+                    <tr>
+                        <td>'.$firstName.'</td>
+                        <td>'.$lastName.'</td>
+                        <td>'.$phone.'</td>
+                        <td>'.$email.'</td>
+                        <td>'.$address.'</td>
+                        <td>'.($member==-1?'':($member==0?'No':'Yes')).'</td>
+                        <td>
+                            <button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit">
+                                <span class="glyphicon glyphicon-pencil"></span>
+                            </button>
+                        </td>
+                        <td>
+                            <button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete">
+                                <span class="glyphicon glyphicon-trash"></span>
+                            </button>
+                        </td>
+                    </tr>
+                ';
+            }
+            ?>
         </table>
     </div>
 </div>
